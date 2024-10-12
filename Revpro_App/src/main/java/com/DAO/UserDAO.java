8df6a100fe6074f0f0b535fc5484872ced18d5bd
Manoj <manojsanikala@gmail.com>
2024-10-12 14:28:30 +0530

    Removed unnecessary files and updated project

--- Revpro_App/src/main/java/com/DAO/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.DAO;
-
-import com.entity.User;
-
-public interface UserDAO {
-	public boolean userRegister(User us);
-	
-	public User login(String email,String password);
-}
--- Revpro_App/src/main/java/com/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import java.util.List;
-import com.entity.Product_Order;
-
-public interface OrderService {
-    List<Product_Order> getOrdersByUserEmail(String email);
-    boolean saveOrders(List<Product_Order> orders);
-}
